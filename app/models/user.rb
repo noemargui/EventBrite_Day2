358cb6618aa079f47c8ffaf26a47730512571515
Noé Marguillard <noe.marguillard@essec.edu>
2019-02-12 14:05:42 +0100

    Problème de session user sur le bouton

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,15 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-         
+
   after_create :welcome_send
 
   has_many :attendances, foreign_key: 'attendee_id'
   has_many :events, foreign_key: 'admin_id'
-
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :description, presence: true
 
   def welcome_send #methode d'envoi d'email
     UserMailer.welcome_email(self).deliver_now
